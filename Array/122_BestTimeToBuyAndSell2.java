21fb63cb9f2d282c13cba5ce901b2e9ee78d89f4
Ayush Jha <aj23.workspace@gmail.com>
2024-10-18 16:58:01 +0530

    Update 122_BestTimeToBuyAndSell2.java

--- Array/122_BestTimeToBuyAndSell2.java
+++ Array/122_BestTimeToBuyAndSell2.java
@@ -3,8 +3,14 @@ class Solution {
 
         int totalProfit = 0;
 
+        // We start iterating from index 1 (the second day) 
+        // to compare each day's price with the previous day's price.
         for(int i=1;i<prices.length;i++){
 
+            // This condition checks if today's price (prices[i]) 
+            // is greater than yesterday's price (prices[i - 1])
+            // If the condition is true, it means we can make a profit 
+            // by buying the stock yesterday and selling it today.
             if(prices[i] > prices[i-1]){
                 int currentProfit = prices[i] - prices[i-1];
                 totalProfit = totalProfit + currentProfit;
